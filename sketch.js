a74f175e42310254e85ce3086f8c5826ba98bdfd
Ivansallas <kiss_ivan@hotmail.com>
2025-05-07 23:43:00 -0300

    ok

--- sketch.js
+++ sketch.js
@@ -1,22 +1,41 @@
-function setup() {
-  createCanvas(600, 600);
-}
-
 let xJogador1 = 0;
 let xJogador2 = 0;
-let xJogador3 = 0;
-let xJogador4 = 0;
+
+function setup() {
+  createCanvas(400, 400);
+}
 
 function draw() {
-  background("gray");
+  if (focused == true) {
+    background("green");
+  } else {
+    background("gray");
+    textSize(15); textStyle(BOLD);
+    text("CLICK NA TELA PARA INICIAR O JOGO", 50, 175);
+  }
+
   textSize(40);
-  text("🥸", xJogador1, 80);
-  text("👽", xJogador2, 220);
-  text("👻", xJogador3, 380);
-  text("🤡", xJogador4, 550);
-  rect(550, 0, 10, width);
-  xJogador1 += random(5);
-  xJogador2 += random(5);
-  xJogador3 += random(5);
-  xJogador4 += random(5);
+  text("😎", xJogador1, 100);
+  text("❤️", xJogador2, 300);
+  rect(350, 0, 10, height); // linha de chegada
+
+  if (xJogador1 > 350) {
+    textSize(20);
+    text("Jogador 1 venceu!", 50, 175);
+    noLoop();
+  }
+  if (xJogador2 > 350) {
+    textSize(20);
+    text("Jogador 2 venceu!", 50, 175);
+    noLoop();
+  }
+}
+
+function keyReleased() {
+  if (key == "a") {
+    xJogador1 += random(20);
+  }
+  if (key == "s") {
+    xJogador2 += random(20);
+  }
 }
